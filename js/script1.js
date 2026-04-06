4bb40292eb8cd5720df7f560218f92d6bd004bfc
luchos-git <trevissan.luciano@gmail.com>
2026-04-06 20:21:09 -0300

    script pepe

--- js/script1.js
+++ js/script1.js
@@ -1,1 +1,9 @@
 console.log("hello");
+
+// titulo = document.querySelector('h1')
+// console.log(titulo);
+// titulo.innerHTML='<i>titulos</i>'
+document.querySelector('h1').innerHTML='<i>tituloss</i>'
+
+document.querySelector('#producto').style.background='red'
+document.querySelector('.texto').innerHTML='nuevo texto'
