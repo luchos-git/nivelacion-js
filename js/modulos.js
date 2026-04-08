baa8866d4dfe2ceeb5be71a7f4b917fa3ab8a781
luchos-git <trevissan.luciano@gmail.com>
2026-04-08 20:02:41 -0300

    arreglos Modulos.js

--- js/modulos.js
+++ js/modulos.js
@@ -1,5 +1,7 @@
 titulo = document.querySelector('header').innerHTML='<strong><i>mi primera pagina con modulos</i></strong>';
 
-const nav='<h1>producto</h1><ul><li><a href="index.html">|Inicio|</a></li><li><a href="productos.html">|Productos|</a></li><li><a href="servicios.html">|Servicios|</a></li></ul>';
+nav = document.querySelector('#menu').innerHTML='<ul><li><a href="index.html">|Inicio|</a></li><li><a href="productos.html">|Productos|</a></li><li><a href="servicios.html">|Servicios|</a></li></ul>';
+// const nav='<h1>producto</h1><ul><li><a href="index.html">|Inicio|</a></li><li><a href="productos.html">|Productos|</a></li><li><a href="servicios.html">|Servicios|</a></li></ul>';
 
-const footer='<p>Luciano Trevissan y Agustin Benavidez </p>';
+footer = document.querySelector('.pie').innerHTML='<p>Luciano Trevissan y Agustin Benavidez </p>';
+// const footer='<p>Luciano Trevissan y Agustin Benavidez </p>';
