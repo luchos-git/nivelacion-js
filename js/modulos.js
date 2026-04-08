6476f2a1597acb6988ad736ed1353c52eb39b2e3
godOmager <callizayazenteno.wendy@tecnica7.edu.ar>
2026-04-08 19:53:50 -0300

    arreglos

---
+++ js/modulos.js
@@ -0,0 +1,5 @@
+titulo = document.querySelector('header').innerHTML='<strong><i>mi primera pagina con modulos</i></strong>';
+
+const nav='<h1>producto</h1><ul><li><a href="index.html">|Inicio|</a></li><li><a href="productos.html">|Productos|</a></li><li><a href="servicios.html">|Servicios|</a></li></ul>';
+
+const footer='<p>Luciano Trevissan y Agustin Benavidez</p>';
