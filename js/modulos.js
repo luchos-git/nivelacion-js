47f63632c4587f292a43d973205199a2f4903ec3
luchos-git <trevissan.luciano@gmail.com>
2026-04-08 20:15:55 -0300

    modulos

--- js/modulos.js
+++ js/modulos.js
@@ -4,5 +4,5 @@ document.querySelector('header').innerHTML=titulo;
 document.querySelector('#menu').innerHTML='<ul><li><a href="index.html">|Inicio|</a></li><li><a href="productos.html">|Productos|</a></li><li><a href="servicios.html">|Servicios|</a></li></ul>';
 // const nav='<h1>producto</h1><ul><li><a href="index.html">|Inicio|</a></li><li><a href="productos.html">|Productos|</a></li><li><a href="servicios.html">|Servicios|</a></li></ul>';
 
-document.querySelector('.pie').innerHTML='<p>Luciano Trevissan y Agustin Benavidez </p>';
+document.querySelector('.pie').innerHTML=' <p>Luciano Trevissan y Agustin Benavidez </p>';
 // const footer='<p>Luciano Trevissan y Agustin Benavidez </p>';
